f8004b951ddd7aef4f47984ae4dc52ac65e2f74f
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-03 05:38:41 -0600

    Branch1 commit1

--- GitHubDesktop/Program.cs
+++ GitHubDesktop/Program.cs
@@ -9,6 +9,9 @@ namespace GitHubDesktop
             Console.WriteLine("Hello World!");
             Console.WriteLine("hey");
             Console.WriteLine("this was afer the merge to GHdesktop");
+
+            Console.WriteLine("this was done after the new Branch1 was made");
+
         }
     }
 }
